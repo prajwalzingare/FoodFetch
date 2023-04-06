8a95e0620130ee337373c79c444c71787adb0df0
prajwal zingare <prajwalzingre@gmail.com>
2023-04-06 08:43:49 +0530

    style:some style added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -40,11 +40,11 @@ function Header() {
           className={`${isOpen ? "" : "hidden"}  w-full md:block md:w-auto`}
           id="navbar-items"
         >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:bg-gray-800  dark:border-gray-700">
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                className="block py-2 pl-3 pr-4 text-white  rounded md:bg-transparent md:hover:text-yellow-200 md:p-0 dark:text-white"
                 aria-current="page"
               >
                 Home
