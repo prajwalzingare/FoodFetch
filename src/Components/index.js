365b290916f8e09069f3e99a43f75e7f1f79d1b5
prajwal zingare <prajwalzingre@gmail.com>
2023-05-08 16:37:02 +0530

    refactor: code refactor

---
+++ src/Components/index.js
@@ -0,0 +1,4 @@
+export { Navbar } from "./Navbar/Navbar";
+export { Body } from "./Body/Body";
+export { Footer } from "./Footer/Footer";
+export { RestaurantCard } from "./RestaurantCard/RestaurantCard";
