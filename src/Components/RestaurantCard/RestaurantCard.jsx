ed1c22b6232f4f17f11fdbcfc5d63d23afe34d51
prajwal zingare <prajwalzingre@gmail.com>
2023-05-08 16:34:33 +0530

    feat:Restaurant Card desighn

--- src/Components/RestaurantCard/RestaurantCard.jsx
+++ src/Components/RestaurantCard/RestaurantCard.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import "./RestaurantCard.css";
 function RestaurantCard() {
-  return <div>RestaurantCard</div>;
+  return (
+    <div>
+      <div className="card-container">
+        <div className="card">
+          <div className="card-image-container">
+            <img
+              src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"
+              alt=""
+              className="card-img"
+            />
+          </div>
+          <div className="card-description">
+            <h5 className="card-title">Card Title</h5>
+            <p>categories</p>
+            <p>place famous for</p>
+            <div className="card-buttons">
+              <button>
+                {" "}
+                <i class="fa-sharp fa-solid fa-star"></i>Rating
+              </button>
+              <p>7.6 kms</p>
+              <p>Price</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default RestaurantCard;
+export { RestaurantCard };
